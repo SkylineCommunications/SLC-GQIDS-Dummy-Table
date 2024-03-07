4f739ad5b959aee2212afcf4592dc996479b72e0
DavidJO <david.joseph@skyline.be>
2024-03-07 17:58:14 +0100

    extended dummy table with option to give row values (keys) as pipe-separate string

--- SLC-GQIDS-Dummy-Table_1/Arguments.cs
+++ SLC-GQIDS-Dummy-Table_1/Arguments.cs
@@ -4,23 +4,29 @@ public class Arguments
 {
 	private readonly GQIIntArgument _numColumnsArg = new GQIIntArgument("Number of Columns") { DefaultValue = 1 };
 	private readonly GQIIntArgument _numRowsArg = new GQIIntArgument("Number of Rows") { DefaultValue = 1 };
+    private readonly GQIStringArgument _rowKeysArg = new GQIStringArgument("Row Keys Pipe Separated") { IsRequired = false, DefaultValue = "" };
 
-	public int NumberOfColumns { get; private set; }
+    public int NumberOfColumns { get; private set; }
 
 	public int NumberOfRows { get; private set; }
 
-	public GQIArgument[] GetArguments()
+    public string RowKeysPipeSeparated { get; private set; }
+
+    public GQIArgument[] GetArguments()
 	{
 		return new GQIArgument[]
 		{
 			_numColumnsArg,
 			_numRowsArg,
-		};
+            _rowKeysArg,
+        };
 	}
 
 	public void ProcessArguments(OnArgumentsProcessedInputArgs args)
 	{
 		NumberOfColumns = args.GetArgumentValue(_numColumnsArg);
 		NumberOfRows = args.GetArgumentValue(_numRowsArg);
-	}
+		RowKeysPipeSeparated = args.GetArgumentValue(_rowKeysArg);
+
+    }
 }
--- SLC-GQIDS-Dummy-Table_1/SLC-GQIDS-Dummy-Table_1.cs
+++ SLC-GQIDS-Dummy-Table_1/SLC-GQIDS-Dummy-Table_1.cs
@@ -1,58 +1,70 @@
+using System;
 using Skyline.DataMiner.Analytics.GenericInterface;
 
 [GQIMetaData(Name = "GQI_Dummy-Table")]
 public class MyDataSource : IGQIDataSource, IGQIInputArguments
 {
-	private readonly Arguments _arguments = new Arguments();
+    private readonly Arguments _arguments = new Arguments();
 
-	public GQIArgument[] GetInputArguments()
-	{
-		return _arguments.GetArguments();
-	}
+    public GQIArgument[] GetInputArguments()
+    {
+        return _arguments.GetArguments();
+    }
 
-	public OnArgumentsProcessedOutputArgs OnArgumentsProcessed(OnArgumentsProcessedInputArgs args)
-	{
-		_arguments.ProcessArguments(args);
-		return new OnArgumentsProcessedOutputArgs();
-	}
+    public OnArgumentsProcessedOutputArgs OnArgumentsProcessed(OnArgumentsProcessedInputArgs args)
+    {
+        _arguments.ProcessArguments(args);
+        return new OnArgumentsProcessedOutputArgs();
+    }
 
-	public GQIColumn[] GetColumns()
-	{
-		var numColumns = _arguments.NumberOfColumns;
+    public GQIColumn[] GetColumns()
+    {
+        var numColumns = _arguments.NumberOfColumns;
+        var columns = new GQIColumn[numColumns];
 
-		var columns = new GQIColumn[numColumns];
+        for (int i = 0; i < numColumns; i++)
+        {
+            columns[i] = new GQIStringColumn($"Column {i + 1}");
+        }
 
-		for (int i = 0; i < numColumns; i++)
-		{
-			columns[i] = new GQIStringColumn($"Column {i + 1}");
-		}
+        return columns;
+    }
 
-		return columns;
-	}
+    public GQIPage GetNextPage(GetNextPageInputArgs args)
+    {
+        var numColumns = _arguments.NumberOfColumns;
+        var numRows = _arguments.NumberOfRows;
 
-	public GQIPage GetNextPage(GetNextPageInputArgs args)
-	{
-		var numColumns = _arguments.NumberOfColumns;
-		var numRows = _arguments.NumberOfRows;
+        var rowKeysPipeSeparated = _arguments.RowKeysPipeSeparated;
+        string[] rowKeys = null;
 
-		var rows = new GQIRow[numRows];
+        if (!String.IsNullOrEmpty(rowKeysPipeSeparated))
+        {
+            rowKeys = rowKeysPipeSeparated.Split('|');
+        }
 
-		for (int row = 0; row < numRows; row++)
-		{
-			var key = $"Row {row + 1}";
-			var cells = new GQICell[numColumns];
+        var rows = new GQIRow[numRows];
 
-			for (int col = 0; col < numColumns; col++)
-			{
-				cells[col] = new GQICell();
-			}
+        for (int row = 0; row < numRows; row++)
+        {
+            var key = $"Row {row + 1}";
+            var cells = new GQICell[numColumns];
 
-			rows[row] = new GQIRow(key, cells);
-		}
+            for (int col = 0; col < numColumns; col++)
+            {
+                cells[col] = new GQICell();
+                if (col == 0 && rowKeys != null && rowKeys[row] != null)
+                {
+                    cells[col].Value = rowKeys[row];
+                }
+            }
 
-		return new GQIPage(rows)
-		{
-			HasNextPage = false,
-		};
-	}
+            rows[row] = new GQIRow(key, cells);
+        }
+
+        return new GQIPage(rows)
+        {
+            HasNextPage = false,
+        };
+    }
 }
